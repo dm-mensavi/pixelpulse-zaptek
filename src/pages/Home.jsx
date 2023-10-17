37ebe0b8c6279e61aa5e2f2f16e6564a52012cee
dm-mensavi <mensavidavid@gmail.com>
2023-10-17 21:06:08 +0000

    Add logo svg

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import heroImage from '../public/heroimage.webp'
+import logo from '../public/PixelPulse.svg'
+
 
 function home() {
   return (
     <div>
       <h1>Home</h1>
+      <img src={logo} width={50} height={50} alt='heroImage' />
       <img src={heroImage} width={512} height={512} alt='heroImage' />
     </div>
   )
