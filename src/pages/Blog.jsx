4544804bb7c6a8b6724199d52122c3dbbda28f8c
dm-mensavi <mensavidavid@gmail.com>
2023-10-26 22:22:30 +0000

    extract blog data from code

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -11,72 +11,83 @@ import blog from '../assets/blog.jpg'
 import blogpic from '../assets/blogpic.jpg'
 
 
+// blogData.js
+
+const blogData = [
+    {
+      id: 1,
+      photo: blog1,
+      date: 'sep 13, 2023',
+      header: 'Mastering the art of email marketing',
+      desc: 'Unlock the secrets to email marketing success with proven strategies and tips for engaging your audience effectively',
+      pic: blog1pic,
+      name: 'John Doe',
+      position: 'Email Specialist',
+      link: '/blog1'
+    },
+    {
+      id: 2,
+      photo: blog2,
+      date: 'sep 13, 2023',
+      header: 'The power of visual storytelling online',
+      desc: 'Explore the impact of visual storytelling in digital content and learn how to leverage its potential.',
+      pic: blog2pic,
+      name: 'Jake Thompson',
+      position: 'Content Strategist',
+      link: '/blog2'
+    },
+    {
+      id: 3,
+      photo: blog,
+      date: 'sep 13, 2023',
+      header: 'The art of crafting compelling content',
+      desc: 'Unlock the secrets to crafting content that captivates your audience and drives meaningful engagement.',
+      pic: blogpic,
+      name: 'Olivia Martinez',
+      position: 'Content Specialist',
+      link: '/blog3'
+    },
+    {
+      id: 4,
+      photo: blog3,
+      date: 'sep 13, 2023',
+      header: 'Effective strategies for boosting online engagement',
+      desc: 'Enhance online engagement with these strategies: audience understanding, quality content, consistency, user-generated content, and more.',
+      pic: blog3pic,
+      name: 'John Smith',
+      position: 'Marketing Strategist',
+      link: '/blog4'
+    },
+  ];
+  
+
  
-const Projects = () => {
+  const Projects = () => {
     return (
-        <div className="Page">
-           
-            <div className="mt-20 md:mt-32 max-w-[70%] mb-32">
-            <h1 className="text-blue-900 font-bold">Blog</h1>
-                <h1 className="lg:text-7xl md:text-6xl sm:text-6xl text-4xl font-extrabold py-5 max-w-2xl lg:w-[90%] md:w-full sm:w-full">Stay informed and  inspired</h1>
-
-                <p>Explore our blog for the latest insights, trends, and strategies in the dynamic world of digital marketing and web development.</p>
-            </div>
-            <div className="flex mt-5 flex-col text-center space-y-5">
-            
-                
-                <Link to="/blog1">
-                <Blogstemplate
-                photo={blog1}
-                date='sep 13,2023'
-                header='Mastering the art of emailing marketing'
-                desc='Unlock the secretes to email marketing success with proven strategies and tips for engaging your audience effectively'
-                pic={blog1pic}
-                name='John Doe'
-                position='Email Specialist'
-                />
-                 </Link>
-
-
-                <Link to="/blog2">
-                <Blogstemplate
-                photo={blog2}
-                date='sep 13,2023'
-                header='The power of visual storytelling online'
-                desc='Explorer the impact of visual storytelling in digital content and learning how to leverage it potential.'
-                pic={blog2pic}
-                name='Jake Thompson'
-                position='Content Strategist'
-                />
-                </Link>
-
-                <Link to="/blog3">
-                <Blogstemplate
-                photo={blog}
-                date='sep 13,2023'
-                header='The art of crafting compelling content'
-                desc='Unlock the secrets to crafting content that captivates your audience
-                and drives meaningful engagement.'
-                pic={blogpic}
-                name='Olivia Martinez'
-                position='Content Specialist'
-                />
-                </Link>
-
-                <Link to="/blog4">
-                <Blogstemplate
-                photo={blog3}
-                date='sep 13,2023'
-                header='Effective strategies for boosting online engagement'
-                desc='Enhance online engagement with these strategies: audience understanding, quality content, consistency, user-generated content, and more.'
-                pic={blog3pic}
-                name='John Smith'
-                position='Marketing Strategist'
-                />
-                </Link>
-            </div>
+      <div className="Page">
+        <div className="mt-20 md:mt-32 max-w-md sm:max-w-[70%] mb-32">
+          <h1 className="text-blue-900 font-bold">Blog</h1>
+          <h1 className="lg:text-7xl md:text-6xl sm:text-6xl text-4xl font-extrabold py-5 max-w-2xl lg:w-[90%] md:w-full sm:w-full">Stay informed and inspired</h1>
+          <p>Explore our blog for the latest insights, trends, and strategies in the dynamic world of digital marketing and web development.</p>
         </div>
+        <div className="flex mt-5 max-w-5xl flex-col text-center space-y-5">
+          {blogData.map((post) => (
+            <Link to={post.link} key={post.id}>
+              <Blogstemplate
+                photo={post.photo}
+                date={post.date}
+                header={post.header}
+                desc={post.desc}
+                pic={post.pic}
+                name={post.name}
+                position={post.position}
+              />
+            </Link>
+          ))}
+        </div>
+      </div>
     );
-};
+  };
+  
+  export default Projects;
  
-export default Projects;
